ce6e47a4d646692264381065c64d4452fff01e4b
Yaroslav Klymko <t3hnar@gmail.com>
2019-08-27 13:17:17 +0200

    bump dependencies

--- build.sbt
+++ build.sbt
@@ -16,7 +16,7 @@ bintrayOrganization := Some("evolutiongaming")
 
 scalaVersion := crossScalaVersions.value.head
 
-crossScalaVersions := Seq("2.12.8")
+crossScalaVersions := Seq("2.12.9")
 
 resolvers += Resolver.bintrayRepo("evolutiongaming", "maven")
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,12 @@ object Dependencies {
 
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.0.8"
   val `cats-par`    = "io.chrisdavenport"   %% "cats-par"    % "0.2.1"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "0.0.18"
-  val smetrics      = "com.evolutiongaming" %% "smetrics"    % "0.0.3"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "0.0.28"
+  val smetrics      = "com.evolutiongaming" %% "smetrics"    % "0.0.4"
 
   object Cats {
     private val version = "1.6.1"
     val core   = "org.typelevel" %% "cats-core"   % version
-    val effect = "org.typelevel" %% "cats-effect" % "1.3.1"
+    val effect = "org.typelevel" %% "cats-effect" % "1.4.0"
   }
 }
