32f3027e59811d0da0e5de40217b6bae3fef22c5
mr-git <8796159+mr-git@users.noreply.github.com>
2019-09-11 21:10:10 +0300

    use Scala 2.12.10 and 2.13.0, SBT 1.3.0, Cats 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,12 @@ import sbt._
 object Dependencies {
 
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.0.8"
-  val `cats-par`    = "io.chrisdavenport"   %% "cats-par"    % "0.2.1"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "0.0.28"
-  val smetrics      = "com.evolutiongaming" %% "smetrics"    % "0.0.4"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "0.0.30"
+  val smetrics      = "com.evolutiongaming" %% "smetrics"    % "0.0.6"
 
   object Cats {
-    private val version = "1.6.1"
+    private val version = "2.0.0"
     val core   = "org.typelevel" %% "cats-core"   % version
-    val effect = "org.typelevel" %% "cats-effect" % "1.4.0"
+    val effect = "org.typelevel" %% "cats-effect" % "2.0.0"
   }
 }
