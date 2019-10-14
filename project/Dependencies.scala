91039b6ec174d22cbd63b28ee328e74120926f0c
Scala Steward <me@scala-steward.org>
2019-10-14 17:02:53 +0200

    Update cats-helper to 1.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val scalatest        = "org.scalatest"       %% "scalatest"      % "3.0.8"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "1.0.3"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "1.0.4"
   val smetrics         = "com.evolutiongaming" %% "smetrics"       % "0.0.6"
   val `kind-projector` = "org.typelevel"        % "kind-projector" % "0.10.3"
 
