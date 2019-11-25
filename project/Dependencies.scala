249885207222b02ab364d4c0e198a885a6f9b093
Yaroslav Klymko <t3hnar@gmail.com>
2019-11-25 10:09:15 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,10 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.8"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "1.1.0"
-  val smetrics = "com.evolutiongaming" %% "smetrics" % "0.0.7"
-  val `kind-projector` = "org.typelevel" % "kind-projector" % "0.11.0"
+  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.0.8"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "1.1.0"
+  val smetrics         = "com.evolutiongaming" %% "smetrics"       % "0.0.8"
+  val `kind-projector` = "org.typelevel"        % "kind-projector" % "0.11.0"
 
   object Cats {
     private val version = "2.0.0"
