d84d62c8810c4f53bf80bf25c947513d5a048169
Scala Steward <me@scala-steward.org>
2020-02-21 02:39:29 +0100

    Update scalatest to 3.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.1.0"
+  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.1.1"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "1.5.2"
   val smetrics         = "com.evolutiongaming" %% "smetrics"       % "0.0.8"
   val `kind-projector` = "org.typelevel"        % "kind-projector" % "0.11.0"
