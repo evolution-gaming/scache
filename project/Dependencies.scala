27c5814c19e9008ee0f9a2d4ad4e5259e605bcf0
Scala Steward <me@scala-steward.org>
2020-08-02 16:16:08 +0200

    Update smetrics to 0.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val scalatest        = "org.scalatest"       %% "scalatest"      % "3.2.0"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "2.0.1"
-  val smetrics         = "com.evolutiongaming" %% "smetrics"       % "0.1.1"
+  val smetrics         = "com.evolutiongaming" %% "smetrics"       % "0.1.2"
   val `kind-projector` = "org.typelevel"        % "kind-projector" % "0.11.0"
 
   object Cats {
