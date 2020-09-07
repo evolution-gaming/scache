c1c87aaf8297c0a083b4e88f52669d4597a4f762
Scala Steward <me@scala-steward.org>
2020-09-07 22:19:14 +0200

    Update cats-effect to 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,6 @@ object Dependencies {
 
   object Cats {
     val core   = "org.typelevel" %% "cats-core"   % "2.1.1"
-    val effect = "org.typelevel" %% "cats-effect" % "2.1.4"
+    val effect = "org.typelevel" %% "cats-effect" % "2.2.0"
   }
 }
