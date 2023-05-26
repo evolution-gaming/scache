eb4d2e0f57d189d746df39af390f79e670da90cf
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-05-26 12:43:19 +0200

    Update scalatest to 3.2.16 (#194)
    Co-authored-by: Ilya Surkov <ilyas9406@yandex.ru>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Dependencies {
 
-  val scalatest        = "org.scalatest"       %% "scalatest"          % "3.2.13"
+  val scalatest        = "org.scalatest"       %% "scalatest"          % "3.2.16"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"        % "3.5.0"
   val smetrics         = "com.evolutiongaming" %% "smetrics"           % "2.0.0"
   val `kind-projector` = "org.typelevel"        % "kind-projector"     % "0.13.2"
