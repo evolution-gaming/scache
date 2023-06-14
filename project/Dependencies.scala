c6212419230bdb527d7f06cc85b93bb783ba760f
Scala Steward <scala_steward@virtuslab.com>
2023-06-14 15:31:33 +0000

    Update cats-helper to 3.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val scalatest        = "org.scalatest"       %% "scalatest"          % "3.2.16"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"        % "3.5.0"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"        % "3.6.0"
   val smetrics         = "com.evolutiongaming" %% "smetrics"           % "2.0.0"
   val `kind-projector` = "org.typelevel"        % "kind-projector"     % "0.13.2"
   val betterMonadicFor = "com.olegpy"          %% "better-monadic-for" % "0.3.1"
