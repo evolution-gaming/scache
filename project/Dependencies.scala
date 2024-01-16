cfafb4e9cbb67599d83722b9e6013af4484db652
Scala Steward <scala_steward@virtuslab.com>
2024-01-16 16:21:29 +0000

    Update cats-effect to 3.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object Dependencies {
 
-  val scalatest        = "org.scalatest"       %% "scalatest"          % "3.2.16"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"        % "3.6.0"
-  val smetrics         = "com.evolutiongaming" %% "smetrics"           % "2.0.0"
-  val `kind-projector` = "org.typelevel"        % "kind-projector"     % "0.13.2"
-  val betterMonadicFor = "com.olegpy"          %% "better-monadic-for" % "0.3.1"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.16"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.6.0"
+  val smetrics = "com.evolutiongaming" %% "smetrics" % "2.0.0"
+  val `kind-projector` = "org.typelevel" % "kind-projector" % "0.13.2"
+  val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % "0.3.1"
 
   object Cats {
-    val core   = "org.typelevel" %% "cats-core"   % "2.9.0"
-    val effect = "org.typelevel" %% "cats-effect" % "3.4.11"
+    val core = "org.typelevel" %% "cats-core" % "2.9.0"
+    val effect = "org.typelevel" %% "cats-effect" % "3.5.3"
   }
 }
