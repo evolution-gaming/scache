c5bf31a27040f8440c86f662d6b5fe8ee385c850
Stas Shevchenko <sshevchenko@evolution.com>
2026-08-05 12:31:40 +0200

    Document cache algorithm and defects it fixes

--- scache/src/main/scala/com/evolution/scache/ExpiredError.scala
+++ scache/src/main/scala/com/evolution/scache/ExpiredError.scala
@@ -2,4 +2,8 @@ package com.evolution.scache
 
 import scala.util.control.NoStackTrace
 
+/**
+ * Failure of a value computation that was taking so long the entry got expired while still loading,
+ * see [[ExpiringCache]].
+ */
 case object ExpiredError extends RuntimeException with NoStackTrace
